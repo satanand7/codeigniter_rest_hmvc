4a122f968f4ab894f19bf52b7945f95f76d2696c
prashant <prashant@DESKTOP-TGAGM0O>
2018-05-08 11:58:12 -0700

    Create new module admin

---
+++ application/modules/admin/controllers/Admin.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Admin
+ *e
+ * @author prashant
+ */
+class Admin extends CI_Model{
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+    }
+}
