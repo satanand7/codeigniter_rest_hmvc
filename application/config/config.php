ba4184621a8357902df3908fbcbab757aadec266
satanand tiwari <satanand.t@gmail.com>
2018-06-22 16:36:31 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://111.118.246.35/radarapp';
+$config['base_url'] = 'http://localhost/radarapp';
 
 /*
 |--------------------------------------------------------------------------
