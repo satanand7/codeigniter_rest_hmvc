02d806ff586792dfec4645ec5ab108d17150695a
satanand tiwari <satanand.t@gmail.com>
2018-08-06 13:38:28 +0530

    Update MY_Form_validation.php

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -9,10 +9,24 @@
 
 class MY_Form_validation extends CI_Form_validation {
 
+   /**
+    * 
+    * @param type $module
+    * @param type $group
+    * @return type
+    * over ride parent class method that can help to work with custom callback function for validation
+    */
+    
     function run($module = '', $group = '') {
         (is_object($module)) AND $this->CI = &$module;
         return parent::run($group);
     }
+    
+    /**
+     * 
+     * @return type
+     * this function is going to help for unset validation data and then we can assign new validation for data.
+     */
 
     public function clear_field_data() {
         $this->_field_data = array();
